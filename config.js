cccc18bf3c7058dc89716c765e0010ced70a0163
Paulina Sikora <paaulinaaas@gmail.com>
2017-09-24 21:32:35 +0200

    done

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '38867812964-77la88jn9boc6omuepe0o1l5e0o63q2s.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'emW4N23ShKCT1DKDmg6_eB6c',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
